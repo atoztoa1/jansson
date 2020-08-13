e53e65a45cd01f844716c15743eb7a40df005e1e
In Kim <atoztoa@telcoware.com>
2020-08-13 19:27:41 +0900

    o. Added GEN AUSF Configure Shell Script

---
+++ configure.gausf.sh
@@ -0,0 +1,6 @@
+./configure \
+--prefix=/home/gausf/MP/ext-libsrc/jansson/release \
+--exec-prefix=/home/gausf/release/CUR/ \
+--libdir=/home/gausf/MP/lib-release \
+--includedir=/home/gausf/MP/include/jansson \
+--enable-shared=no 
