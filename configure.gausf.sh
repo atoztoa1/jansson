8623b0d5b1e0b1bbfabe415b72690265c31d0846
In Kim <atoztoa@telcoware.com>
2020-08-14 10:42:15 +0900

    o. Remove Unnecessarary Configure Options

--- configure.gausf.sh
+++ configure.gausf.sh
@@ -1,6 +1,3 @@
 ./configure \
 --prefix=/home/gausf/MP/ext-libsrc/jansson/release \
---exec-prefix=/home/gausf/release/CUR/ \
---libdir=/home/gausf/MP/lib-release \
---includedir=/home/gausf/MP/include/jansson \
 --enable-shared=no 
